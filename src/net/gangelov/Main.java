46956d9d201dea03f6f13af59cfa335a68c69271
Georgy Angelov <georgyangelov@gmail.com>
2015-12-22 23:23:51 +0200

    Initial commit of skeleton project

---
+++ src/net/gangelov/Main.java
@@ -0,0 +1,8 @@
+package net.gangelov;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/net/gangelov/Transporter.java
@@ -0,0 +1,16 @@
+package net.gangelov;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Transporter extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
