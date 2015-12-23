90372bc71214de86332d505bd1451a26a47bbaf3
Georgy Angelov <georgyangelov@gmail.com>
2015-12-23 11:16:02 +0200

    Add ability to run Async tasks

--- src/net/gangelov/Main.java
+++ src/net/gangelov/Main.java
@@ -1,15 +1,20 @@
 package net.gangelov;
 
+import net.gangelov.transporter.concurrency.Async;
 import net.gangelov.transporter.network.NatTraversal;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
         System.out.println("Hello");
 
         NatTraversal nat = new NatTraversal();
 
-        System.out.println("Your public address is " + nat.getPublicAddress());
+        Future<String> ipFuture = Async.run(() -> nat.getPublicAddress());
+
+        System.out.println("Your public address is " + ipFuture.get());
     }
 }
---
+++ src/net/gangelov/transporter/concurrency/Async.java
@@ -0,0 +1,19 @@
+package net.gangelov.transporter.concurrency;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class Async {
+    @FunctionalInterface
+    public interface CheckedSupplier<R> {
+        R get() throws Exception;
+    }
+
+    public static <T> Future<T> run(CheckedSupplier<T> function) {
+        return Executors.newSingleThreadExecutor().submit(() -> function.get());
+    }
+}
