07e5983b8d4b391f8cfcc75a628e0261a3fede6f
Georgy Angelov <georgyangelov@gmail.com>
2015-12-23 21:28:09 +0200

    Add ClientHandler skeleton

---
+++ src/net/gangelov/transporter/network/ClientHandler.java
@@ -0,0 +1,39 @@
+package net.gangelov.transporter.network;
+
+import net.gangelov.transporter.network.protocol.Packet;
+import net.gangelov.transporter.network.protocol.ProtocolHandler;
+import net.gangelov.transporter.network.protocol.server.ClientConnectionHandler;
+import net.gangelov.transporter.network.protocol.serverPackets.HeadersPacket;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ClientHandler extends ProtocolHandler {
+    private int port;
+
+    public ClientHandler(int port) throws IOException {
+        this.port = port;
+    }
+
+    @Override
+    public synchronized void socketConnected(ClientConnectionHandler connectionHandler) throws IOException {
+        super.socketConnected(connectionHandler);
+    }
+
+    @Override
+    public void packetReceived(Packet packet) throws IOException {
+        if (packet.opcode == HeadersPacket.OPCODE) {
+            // TODO: Create a file, open a few more connections, start receiving on them, then send AcceptFilePacket
+        } else {
+            throw new RuntimeException("Unknown packet opcode " + packet.opcode);
+        }
+    }
+
+    public void start() throws IOException {
+        // TODO: Connect to the server
+    }
+
+    public void shutdown() {
+        // TODO: Disconnect from the server
+    }
+}
